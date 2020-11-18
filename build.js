776a7564b02107b8563a8d18a3c8fc646170da7d
Codixer <stefano@stefanocoding.me>
2020-11-18 10:31:41 +0100

    AAAAA

--- build.js
+++ build.js
@@ -18,8 +18,8 @@ function getCurrentPlatform(){
 builder.build({
     targets: (process.argv[2] != null && Platform[process.argv[2]] != null ? Platform[process.argv[2]] : getCurrentPlatform()).createTarget(),
     config: {
-        appId: 'mbt-launcher',
-        productName: 'Magic Building Technology',
+        appId: 'mbt-pack',
+        productName: 'MBT-Pack',
         artifactName: '${productName}-setup-${version}.${ext}',
         copyright: 'Copyright © 2018-2020 Daniel Scalzi',
         directories: {
